d552a3fb443a962db04acb49fe6ce8c2a85a4902
front-lucy <devfront@jaedam.com>
2025-04-04 10:36:25 +0900

    Typography token registration

---
+++ src/tokens/typography.ts
@@ -0,0 +1,114 @@
+export const typography = {
+  fontFamily: `'Noto Sans KR', sans-serif`,
+
+  headings: {
+    h1: {
+      fontSize: "40px",
+      lineHeight: "140%",
+      fontWeight: "700",
+      letterSpacing: "-0.02em",
+    },
+    h2: {
+      fontSize: "36px",
+      lineHeight: "140%",
+      fontWeight: "700",
+      letterSpacing: "-0.02em",
+    },
+    h3: {
+      fontSize: "32px",
+      lineHeight: "140%",
+      fontWeight: "700",
+      letterSpacing: "-0.02em",
+    },
+    h4: {
+      fontSize: "28px",
+      lineHeight: "140%",
+      fontWeight: "700",
+      letterSpacing: "-0.02em",
+    },
+    h5: {
+      fontSize: "24px",
+      lineHeight: "140%",
+      fontWeight: "700",
+      letterSpacing: "-0.02em",
+    },
+    h6: {
+      fontSize: "20px",
+      lineHeight: "140%",
+      fontWeight: "700",
+      letterSpacing: "-0.02em",
+    },
+    h7: {
+      fontSize: "18px",
+      lineHeight: "140%",
+      fontWeight: "700",
+      letterSpacing: "-0.02em",
+    },
+    h8: {
+      fontSize: "16px",
+      lineHeight: "140%",
+      fontWeight: "700",
+      letterSpacing: "-0.02em",
+    },
+  },
+
+  paragraphs: {
+    p1: {
+      fontSize: "28px",
+      lineHeight: "140%",
+      fontWeight: "400",
+      letterSpacing: "-0.02em",
+    },
+    p2: {
+      fontSize: "24px",
+      lineHeight: "140%",
+      fontWeight: "400",
+      letterSpacing: "-0.02em",
+    },
+    p3: {
+      fontSize: "20px",
+      lineHeight: "140%",
+      fontWeight: "400",
+      letterSpacing: "-0.02em",
+    },
+    p4: {
+      fontSize: "18px",
+      lineHeight: "140%",
+      fontWeight: "400",
+      letterSpacing: "-0.02em",
+    },
+    p5: {
+      fontSize: "16px",
+      lineHeight: "140%",
+      fontWeight: "400",
+      letterSpacing: "-0.02em",
+    },
+    p6: {
+      fontSize: "14px",
+      lineHeight: "140%",
+      fontWeight: "400",
+      letterSpacing: "-0.02em",
+    },
+    p7: {
+      fontSize: "12px",
+      lineHeight: "140%",
+      fontWeight: "400",
+      letterSpacing: "-0.02em",
+    },
+  },
+
+  captions: {
+    caption1: {
+      fontSize: "10px",
+      lineHeight: "140%",
+      fontWeight: "400",
+      letterSpacing: "-0.02em",
+    },
+    caption2: {
+      fontSize: "9px",
+      lineHeight: "140%",
+      fontWeight: "400",
+      letterSpacing: "-0.02em",
+    },
+  },
+};
