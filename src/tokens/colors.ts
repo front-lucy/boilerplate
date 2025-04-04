adc564d99f6195e9b8b6ecd6598b9e06112d6008
front-lucy <devfront@jaedam.com>
2025-04-04 10:35:58 +0900

    Registering color tokens

---
+++ src/tokens/colors.ts
@@ -0,0 +1,33 @@
+export const colors = {
+  // Primary
+  primary: '#FF008A',
+  primary05: '#FFF2F9',
+  primary10: '#FFE5F3',
+  primary20: '#FFCCE8',
+  primary30: '#FFB2DC',
+  primary40: '#FF99D0',
+  primary50: '#FF80C4',
+  primary60: '#FF66B9',
+  primary70: '#FF4DAD',
+  primary80: '#FF33A1',
+  primary90: '#FF1A95',
+  primary_black10: '#E5007C',
+  // Grayscale
+  black: '#131313',
+  gray6: '#333333',
+  gray5: '#666666',
+  gray4: '#999999',
+  gray3: '#CCCCCC',
+  gray2_5: '#DDDDDD',
+  gray2: '#EEEEEE',
+  gray1: '#FAFAFA',
+  white: '#FFFFFF',
+
+  // Secondary
+  secondary1: '#16FD95',
+  secondary2: '#5533FF',
+  secondary2_20: '#DDD6FF',
+
+  // Error
+  error: '#DF1D1D',
+}
